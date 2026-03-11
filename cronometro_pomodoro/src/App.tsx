fe7a8b7850d8550c5b59c3bf42813085820551af
Lavínia <lavizsenai@gmail.com>
2026-03-11 10:40:22 -0300

    continuação do projeto

--- cronometro_pomodoro/src/App.tsx
+++ cronometro_pomodoro/src/App.tsx
@@ -1,14 +1,25 @@
 import './styles/global.css'
 import './styles/theme.css'
 import { Heading } from './components/Heading'
-import {Container} from './components/Container'
+import { Container } from './components/Container'
+import { Logo } from './components/Logo'
+import { Menu } from './components/Menu'
 
 export function App() {
   return (
     <>
       <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu/>
+      </Container>
+
+      <Container>
         <Heading>Logo</Heading>
       </Container>
+      
       <Container>
         <Heading>Menu</Heading>
       </Container>
--- cronometro_pomodoro/src/components/Logo/index.tsx
+++ cronometro_pomodoro/src/components/Logo/index.tsx
@@ -1,12 +1,12 @@
-import {TimerIcon} from 'lucide-react'
+import { TimerIcon } from 'lucide-react'
 import styles from './styles.module.css'
 
-export function Logo(){
-    return(
-        <div>
-            <TimerIcon size={32} color="#fff"/>
-             <span className={styles.logo}>Pomodoro</span>
-        </div>
-
+export function Logo() {
+    return (
+        <>
+            <div className={styles.logo}>
+                <a className={styles.logoLink} href="#"><TimerIcon/><span>Cronometro</span></a>
+            </div>
+        </>
     )
 }
